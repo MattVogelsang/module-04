2d74c02f29df61089ff496d6753f43ab25f20fb2
MattVogelsang <Vogelsangmatt@gmail.com>
2024-08-29 21:42:19 -0400

    Updated html

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,5 +1,8 @@
 const section= document.querySelector("section"); 
 const blogs=JSON.parse(localStorage.getItem("blogs")) || [];
+const body = document.querySelector("body");
+const theme =document.querySelector("#theme");
+let image =document.querySelector("#image");
 function loadBlogs(){
     section.textContent="";
 
@@ -63,6 +66,32 @@ section.innerHTML=`<article>
 // article.appendChild( pPost);
 // section.appendChild(article);
 
+
+
+
+// const body = document.querySelector("body");
+// const theme =document.querySelector("#theme");
+// let image =document.querySelector("#image");
+
+let mode = "light";
+
+// Listen for a click event on toggle switch
+theme.addEventListener("click", function () {
+  // If mode is dark, apply light background
+  if (mode === "light") {
+    mode = "dark";
+    body.setAttribute("class", "dark");
+    image.src=image.dataset.moon;
+    image.dataset.status="dark"
+  }
+  // If mode is light, apply dark background
+  else {
+    mode = "light";
+    body.setAttribute("class", "light");
+    image.src=image.dataset.sun;
+    image.dataset.status="dark"
+  }
+});
 
 }
 
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,18 +1,25 @@
-const themeSwitcher = document.querySelector('#theme-switcher');
-const container = document.querySelector('.container');
 
-let mode = 'dark';
+const body = document.querySelector("body");
+const theme =document.querySelector("#theme");
+let image =document.querySelector("#image");
+
+let mode = "light";
 
 // Listen for a click event on toggle switch
-themeSwitcher.addEventListener('click', function () {
+theme.addEventListener("click", function () {
   // If mode is dark, apply light background
-  if (mode === 'dark') {
-    mode = 'light';
-    container.setAttribute('class', 'light');
+  if (mode === "light") {
+    mode = "dark";
+    body.setAttribute("class", "dark");
+    image.src=image.dataset.moon;
+    image.dataset.status="dark"
   }
   // If mode is light, apply dark background
   else {
-    mode = 'dark';
-    container.setAttribute('class', 'dark');
+    mode = "light";
+    body.setAttribute("class", "light");
+    image.src=image.dataset.sun;
+    image.dataset.status="dark"
   }
 });
+
